c64b76f18407ac466e05b1f2e0ecb6610e2d83d8
Maksim <67355664+Drak0ndr@users.noreply.github.com>
2025-05-30 14:28:23 +0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,17 @@ import { baseUrl } from './app/api/baseUrl';
 export const middleware = async (request: NextRequest) => {
   const token = request.cookies.get('token')?.value;
   const response = NextResponse.next();
-  // console.log('mid',token)
+  console.log('mid',token)
   if (['/devlog', '/uabwdiauwbdiauywbdiauwybd'].includes(request.nextUrl.pathname)) {
     console.log('mid', request.nextUrl.pathname)
     try {
+      console.log('jwt req', `${baseUrl}/validateJWT`)
       const jwtData = await (
         await fetch(`${baseUrl}/validateJWT`, {
           headers: { Authorization: `Bearer ${token}` },
         })
       ).json();
-
+      console.log('jwt data', jwtData)
       if (!jwtData.valid) {
         return NextResponse.redirect(new URL('/', request.url));
       }
